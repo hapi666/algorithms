0dcb0b40e8ba39a89d464fea653b40e996bba68b
hapi666 <281127500@qq.com>
2018-10-27 23:47:12 +0800

    insert-sort

---
+++ sort/insert-sort/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func insertSort(a []int) []int {
+	l:=len(a)
+	for i:=1;i<l ;i++ {
+		for j:=i;j>0 ;j--  {
+			//temp:=a[j]
+			if a[j]<a[j-1] {
+				a[j],a[j-1]=a[j-1],a[j]
+			}
+		}
+	}
+	temp:=make([]int,l)
+	copy(temp,a)
+	return temp
+}
+
+func main(){
+	var a=[]int{27, 38, 12, 39, 27, 16}
+	newa:=insertSort(a)
+	fmt.Println(newa)
+}
