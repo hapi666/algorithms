6011a370c8746a9dec9a4683b362a73380642026
hapi666 <281127500@qq.com>
2018-10-28 17:11:49 +0800

    format code

--- sort/insert-sort/main.go
+++ sort/insert-sort/main.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
-func insertSort(a []int) []int {
+func insertSort(a []int) {
 	l := len(a)
+	if l <= 1 {
+		return
+	}
 	for i := 1; i < l; i++ {
 		for j := i; j > 0; j-- {
 			if a[j] < a[j-1] {
@@ -11,13 +14,10 @@ func insertSort(a []int) []int {
 			}
 		}
 	}
-	temp := make([]int, l)
-	copy(temp, a)
-	return temp
 }
 
 func main() {
-	var a = []int{27, 38, 12, 39, 27, 16}
-	newa := insertSort(a)
-	fmt.Println(newa)
+	var b = []int{27, 38, 12, 39, 27, 16}
+	insertSort(b)
+	fmt.Println(b)
 }
